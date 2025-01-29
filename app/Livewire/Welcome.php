06b4acce2cd95a04f42ade4ea42c8aa30e70bdf2
Remily <87406167+norangmoran@users.noreply.github.com>
2025-01-29 17:40:36 +0900

    라이브와이어 버튼액션 예제

--- app/Livewire/Welcome.php
+++ app/Livewire/Welcome.php
@@ -7,6 +7,15 @@ use Livewire\Component;
 class Welcome extends Component
 {
     public $post;
+    public $clicked;
+    public function mount()
+    {
+        $this->clicked = "클릭 전";
+    }
+    public function click()
+    {
+        $this->clicked = "클릭 후";
+    }
     public function render()
     {
         return view('livewire.welcome',["post" => $this->post]);
--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -2,4 +2,7 @@
     <h1>{{ $post }}</h1>
     <p>----------------구분줄----------------</p>
     <input type="text" wire:model.live="post">
+    <p>----------------구분줄----------------</p>
+    <h1>{{ $clicked }}</h1>
+    <button wire:click="click">클릭이벤트</button>
 </div>
