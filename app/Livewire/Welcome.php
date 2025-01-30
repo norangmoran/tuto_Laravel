26cf095277bb7e7cbb2152bd5c1a4febc061de77
Remily <87406167+norangmoran@users.noreply.github.com>
2025-01-31 00:33:38 +0900

    라이브와이어 유효성 검사 예제

--- app/Livewire/Welcome.php
+++ app/Livewire/Welcome.php
@@ -10,6 +10,27 @@ class Welcome extends Component
     public $clicked;
     public $toggle = false;
 
+    public $name;
+    public $email;
+    public $password;
+    protected $rules = [
+        "name" => "required|string|max:4",
+        "email" => "required|email",
+        "password" => "required|min:8"
+    ];
+    protected $messages = [
+        "name.required" => "필수 입력 사항입니다.",
+        "name.max" => "이름은 4글자 이하로 입력해야 합니다.",
+        "email.required" => "필수 입력 사항입니다.",
+        "email.email" => "이메일 형식이 아닙니다.",
+        "password.required" => "필수 입력 사항입니다.",
+        "password.min" => "비밀번호는 8자 이상 입력해야 합니다."
+    ];
+
+    public function validation()
+    {
+        $this->validate();
+    }
     public function mount()
     {
         $this->clicked = "클릭 전";
--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -12,4 +12,23 @@
         <h1>OFF</h1>
     @endif
     <button wire:click="$toggle('toggle')">토글이벤트</button>
+    <p>----------------구분줄----------------</p>
+    <form wire:submit.prevent="validation">{{-- 기본 이벤트 취소되며 validation 함수 실행 --}}
+        <input type="text" placeholder="이름을 입력하세요" wire:model="name">
+        @error('name')
+            <span>{{ $message }}</span>
+        @enderror
+        <br>
+        <input type="text" placeholder="이메일을 입력하세요" wire:model="email">
+        @error('email')
+            <span>{{ $message }}</span>
+        @enderror
+        <br>
+        <input type="password" placeholder="비밀번호를 입력하세요" wire:model="password">
+        @error('password')
+            <span>{{ $message }}</span>
+        @enderror
+        <br>
+        <button>제출</button>
+    </form>
 </div>
